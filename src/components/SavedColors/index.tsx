a790c491be68a81ee87a737c8f9ed048bc7054b1
Renan Silva <renansupermanox@gmail.com>
2025-09-28 12:01:58 +0100

    indentation fix

--- src/components/SavedColors/index.tsx
+++ src/components/SavedColors/index.tsx
@@ -1,7 +1,15 @@
 import { useSelectedColor } from "../../context/SelectedColorProvider";
 import { useEffect, useRef, useState } from "react";
 import type { Color } from "chroma-js";
-import { S_Container, S_OptionsButton, S_SaveButton, S_SavedColorItem, S_SavedColorOptions, S_SavedColorsList, S_SmallColorPreview } from "./styles";
+import {
+  S_Container,
+  S_OptionsButton,
+  S_SaveButton,
+  S_SavedColorItem,
+  S_SavedColorOptions,
+  S_SavedColorsList,
+  S_SmallColorPreview,
+} from "./styles";
 import { Pencil, Save, Trash2 } from "lucide-react";
 import { useTheme } from "styled-components";
 import type { SavedColor } from "../../types/SavedColor";
@@ -10,7 +18,7 @@ import { AnimatePresence } from "motion/react";
 const ICON_SETTINGS = {
   size: 18,
   strokeWidth: 2,
-}
+};
 
 export default function SavedColors() {
   const { color, setColor } = useSelectedColor();
@@ -26,41 +34,43 @@ export default function SavedColors() {
         top: 0,
       });
     }
-  }
+  };
 
   const handleSaveNewColor = () => {
-    setSavedColors(prev => {
+    setSavedColors((prev) => {
       const copy = [...prev];
       copy.unshift({
         color,
-        id: Math.floor(performance.now())
+        id: Math.floor(performance.now()),
       });
       return copy;
     });
 
     scrollTop();
-  }
+  };
 
   const handleRemoveSavedColor = (id: number) => {
-    setSavedColors(prev => prev.filter((savedColor) => savedColor.id !== id));
-  }
+    setSavedColors((prev) => prev.filter((savedColor) => savedColor.id !== id));
+  };
 
   const handleSetAsCurrentColor = (color: Color) => {
     setColor(color);
-  }
+  };
 
   const handleEditColor = (savedColor: SavedColor) => {
     setColor(savedColor.color);
     setEditingColorId(savedColor.id);
-  }
+  };
 
   const handleSaveEditedColor = () => {
-    setSavedColors(prev => {
-      const copy = prev.filter((savedColor) => editingColorId !== savedColor.id);
-      
+    setSavedColors((prev) => {
+      const copy = prev.filter(
+        (savedColor) => editingColorId !== savedColor.id
+      );
+
       copy.unshift({
         color,
-        id: editingColorId!
+        id: editingColorId!,
       });
 
       return copy;
@@ -68,14 +78,14 @@ export default function SavedColors() {
 
     scrollTop();
     setEditingColorId(undefined);
-  }
+  };
 
   useEffect(() => {
     const stopEditingColor = (e: KeyboardEvent) => {
       if (e.key === "Escape") {
         setEditingColorId(undefined);
       }
-    }
+    };
 
     window.addEventListener("keydown", stopEditingColor);
     return () => window.removeEventListener("keypress", stopEditingColor);
@@ -98,12 +108,12 @@ export default function SavedColors() {
       <S_SavedColorsList ref={ulRef}>
         <AnimatePresence mode="popLayout">
           {savedColors.map((savedColor) => (
-            <S_SavedColorItem 
+            <S_SavedColorItem
               key={savedColor.id}
               layout
-              initial={{ scale: .5, opacity: 0 }}
+              initial={{ scale: 0.5, opacity: 0 }}
               animate={{ scale: 1, opacity: 1 }}
-              exit={{ scale: .5, opacity: 0 }}
+              exit={{ scale: 0.5, opacity: 0 }}
               $isEditingThisColorItem={
                 !editingColorId ? undefined : editingColorId === savedColor.id
               }
