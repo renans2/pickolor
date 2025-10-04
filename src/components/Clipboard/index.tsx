53588804e575ad9b92109b42f23c4269ee5eaccd
Renan Silva <renansupermanox@gmail.com>
2025-10-04 15:43:32 +0100

    clipboard functionality

--- src/components/Clipboard/index.tsx
+++ src/components/Clipboard/index.tsx
@@ -1,8 +1,52 @@
-import { S_Container } from "./styles";
+import { useState } from "react";
+import { useColorPicker } from "../../context/ColorPickerProvider";
+import { S_Container, S_Select, S_SelectedColorModeToCopy } from "./styles";
+import type { ColorMode } from "../../types/ColorMode";
+import { Check, Copy } from "lucide-react";
+
+type ClipboardColorMode = Exclude<ColorMode, "HSV"> | "HSL";
 
 export default function Clipboard() {
+  const { color } = useColorPicker();
+  const [colorMode, setColorMode] = useState<ClipboardColorMode>("RGB");
+  const [copied, setCopied] = useState(false);
+
+  const toCopy = colorMode === "HEX" 
+    ? color.hex() 
+    : color.css(colorMode.toLowerCase() as "rgb" | "hsl");
+
+  const handleCopyToClipboard = async () => {
+    if (!copied) {
+      await navigator.clipboard.writeText(toCopy);
+      setCopied(true);
+      setTimeout(() => setCopied(false), 2000);
+    }
+  }
+
   return (
     <S_Container>
+      <S_Select 
+        id="colorModeClipboard" 
+        value={colorMode}
+        onChange={(e) => setColorMode(e.target.value as ClipboardColorMode)}
+      >
+        <option value="RGB">RGB</option>
+        <option value="HSL">HSL</option>
+        <option value="HEX">HEX</option>
+      </S_Select>
+
+      <S_SelectedColorModeToCopy 
+        onClick={handleCopyToClipboard}
+        $copied={copied}
+      >
+        <span>{toCopy}</span>
+        
+        {copied ? (
+          <Check size={20} />
+        ) : (
+          <Copy size={20} />
+        )}
+      </S_SelectedColorModeToCopy>
     </S_Container>
   );
 }
--- src/components/Clipboard/styles.ts
+++ src/components/Clipboard/styles.ts
@@ -2,7 +2,33 @@ import styled from "styled-components";
 import { S_MainSurface } from "../../base/MainSurface";
 
 export const S_Container = styled(S_MainSurface)`
-  width: 100%;
-  height: 100px;
   grid-area: clipboard;
+  padding: 10px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-evenly;
+`;
+
+export const S_Select = styled.select`
+  border: ${({ theme }) => theme.border};
+  border-radius: ${({ theme }) => theme.rounded.sm};
+  color: ${({ theme }) => theme.colors.textPrimary};
+  width: 100%;
+  padding: 5px;
+`;
+
+export const S_SelectedColorModeToCopy = styled.div<{ $copied: boolean }>`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  color: ${({ theme }) => theme.colors.textPrimary};
+  font-size: 0.9rem;
+  padding: 7px;
+  border-radius: 10px;
+
+  cursor: ${({ $copied }) => $copied ? "default" : "pointer"};
+
+  &:hover {
+    background-color: rgba(0, 0, 0, .05);
+  }
 `;
