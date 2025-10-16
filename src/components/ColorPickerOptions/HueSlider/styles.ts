d87561ff96fa1350e4d26600ff226c09e0399ff9
Renan Silva <renansupermanox@gmail.com>
2025-10-16 15:03:17 +0100

    added firefox compatible properties to hue slider

--- src/components/ColorPickerOptions/HueSlider/styles.ts
+++ src/components/ColorPickerOptions/HueSlider/styles.ts
@@ -4,6 +4,7 @@ export const S_HueSlider = styled.input`
   width: 100%;
   appearance: none;
   cursor: grab;
+  background: transparent;
 
   &:active {
     cursor: grabbing;
@@ -26,16 +27,45 @@ export const S_HueSlider = styled.input`
   }
 
   &::-webkit-slider-thumb {
-    background-color: rgba(255, 255, 255, .75);
-    backdrop-filter: blur(3px);
-    box-shadow: ${({ theme }) => theme.shadow.detail};
-    border: ${({ theme }) => theme.border};
-
     -webkit-appearance: none;
     appearance: none;
     width: 30px;
     height: 30px;
     border-radius: 100%;
-    margin-top: -10px
+    background-color: rgba(255, 255, 255, 0.75);
+    backdrop-filter: blur(3px);
+    box-shadow: ${({ theme }) => theme.shadow.detail};
+    border: ${({ theme }) => theme.border};
+    margin-top: -10px;
+  }
+
+  &::-moz-range-track {
+    box-shadow: ${({ theme }) => theme.shadow.detail};
+    height: 10px;
+    border-radius: 100px;
+    background: linear-gradient(
+      to right, 
+      hsl(0, 100%, 50%), 
+      hsl(60, 100%, 50%), 
+      hsl(120, 100%, 50%), 
+      hsl(180, 100%, 50%), 
+      hsl(240, 100%, 50%), 
+      hsl(300, 100%, 50%), 
+      hsl(360, 100%, 50%)
+    );
+  }
+
+  &::-moz-range-thumb {
+    width: 30px;
+    height: 30px;
+    border-radius: 100%;
+    background-color: rgba(255, 255, 255, 0.75);
+    backdrop-filter: blur(3px);
+    box-shadow: ${({ theme }) => theme.shadow.detail};
+    border: ${({ theme }) => theme.border};
+  }
+
+  &::-moz-focus-outer {
+    border: 0;
   }
 `;
